23fd4dc6fbd556481b3893bffb571b69891b151c
Faris <farissharayri@gmail.com>
2024-07-30 23:09:03 -0400

    complete exercise

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 import express from 'express'
 import {collectibles} from './data/collectibles.js'
-
+import { shoes } from './data/shoes.js'
 // create Express app
 
 
@@ -54,4 +54,35 @@ app.get('/collectibles/:index', function(req, res) {
   else{
     res.send(`This item is not yet in stock. Check back soon!`)
   }
+})
+
+
+app.get('/shoes', function(req,res){
+  let filterMinPrice = false
+  let filterMaxPrice = false
+  let filterType = false
+  if (req.query.minPrice) filterMinPrice=true
+  if (req.query.maxPrice) filterMaxPrice=true
+  if (req.query.type) filterType=true
+
+  let filteredShoes = shoes.filter(shoe=>{
+    
+    if (filterMinPrice&& filterMaxPrice&& filterType)
+      return (shoe.price> req.query.minPrice  && shoe.price < req.query.maxPrice&& shoe.type===req.query.type)
+    else if (filterMaxPrice&&filterMinPrice)
+      return (shoe.price> req.query.minPrice  && shoe.price < req.query.maxPrice)
+    else if (filterMaxPrice&&filterType)
+      return (shoe.price < req.query.maxPrice&& shoe.type===req.query.type)
+    else if (filterMinPrice&&filterType)
+      return (shoe.price > req.query.minPrice&& shoe.type===req.query.type)
+    else if (filterMaxPrice) 
+      return shoe.price < req.query.maxPrice
+    else if (filterMinPrice) 
+      return shoe.price > req.query.minPrice
+    else if (filterType) 
+      return shoe.type === req.query.type
+    else 
+      return true
+  })
+  res.send(filteredShoes)
 })
